6a7dc45d4464d07504d00ea60607165f02986f3c
Сергей Платонов <platonovpochta@gmail.com>
2023-06-02 22:40:43 +0200

    Модифицировал конструктор Channel Добавил в класс Channel следующие методы: класс-метод get_service() метод to_json()

--- src/channel.py
+++ src/channel.py
@@ -6,13 +6,44 @@ import os
 class Channel:
     """Класс для ютуб-канала"""
     api_key = os.getenv('API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self._channel_id = channel_id
+
+        self.info = Channel.info(self._channel_id)
+
+        self.title = self.info['items'][0]['snippet']['title']
+        self.channel_info = self.info['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/{self.info['items'][0]['snippet']['customUrl']}"
+        self.subscriber_count = self.info['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.info['items'][0]['statistics']['videoCount']
+        self.view_count = self.info['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        youtube = build('youtube', 'v3', developerKey=Channel.api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        youtube = build('youtube', 'v3', developerKey=Channel.info(self._channel_id).api_key)
+        channel = youtube.channels().list(id=self._channel_id, part='snippet,statistics').execute()
         return print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @staticmethod
+    def info(channel_id):
+        channel = Channel.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        data = json.dumps(channel, indent=2, ensure_ascii=False)
+        return json.loads(data)
+
+    @classmethod
+    def get_service(cls):
+        return Channel.youtube
+
+    def to_json(self, data):
+        information = {'id канала': self._channel_id,
+                       'название канала': self.title,
+                       'описание канала': self.channel_info,
+                       'ссылка на канал': self.url,
+                       'количество подписчиков': self.subscriber_count,
+                       'количество видео': self.video_count,
+                       'общее количество просмотров': self.view_count}
+        with open(data, 'w', encoding='utf-8') as file:
+            json.dump(information, file, indent=4, ensure_ascii=False)
